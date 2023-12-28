0263a039543e4de7b5c3726d3eb56b4eb1a03d58
eric gimenes <gimenes.eric@live.com>
2023-12-28 15:57:35 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        double numP = 585555556;
+        System.out.println(numP);
+        float numO = (float) numP;
+        System.out.println(numO);
+
+        double media = Double.valueOf(8);
+
+    }
+}
