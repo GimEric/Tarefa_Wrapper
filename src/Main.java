e6c45156ed4e2e3a4340883daa006ae43be2cfb3
eric gimenes <gimenes.eric@live.com>
2023-12-28 19:32:34 -0300

    MODIFICADO X

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import javax.swing.plaf.synth.SynthScrollBarUI;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        double numP = 585555556;
+        double numP = 58.5555556;
         System.out.println(numP);
         float numO = (float) numP;
         System.out.println(numO);
 
-        double media = Double.valueOf(8);
+
+        double media = Double.valueOf(58.5555556);
 
     }
 }
